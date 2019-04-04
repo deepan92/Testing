04355e05de1d17cbe421799988d5703ba35a0a14
Fatlum Aliu <fatlumaliu@yahoo.com>
2019-04-04 14:36:51 +0200

    added gitignore

---
+++ src/test/java/stepdefs/websitesteps/OnBoardingStepDefinitions.java
@@ -0,0 +1,10 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+
+public class OnBoardingStepDefinitions {
+    @And("^Filter by \"([^\"]*)\"$")
+    public void filterBy(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
